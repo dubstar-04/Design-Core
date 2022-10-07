eff848abf523bea182887cb3adfab00f268c9693
Daniel Wood <s.d.wood.82@googlemail.com>
2022-10-07 16:43:03 +0100

    set the correct colour on rectangles

--- entities/rectangle.js
+++ entities/rectangle.js
@@ -1,6 +1,7 @@
 import { Point } from './point.js'
 import { Utils } from '../lib/utils.js'
 import { Intersection } from '../lib/intersect.js'
+import { Colours } from '../lib/colours.js'
 
 export class Rectangle {
     constructor(data) {
@@ -100,7 +101,8 @@ export class Rectangle {
           ctx.beginPath()
         }catch{ // Cairo
           ctx.setLineWidth(this.lineWidth / scale);
-          ctx.setSourceRGB(0.8, 0.0, 0.0);
+          var rgbColour = Colours.getRGBColour(colour)
+          ctx.setSourceRGB(rgbColour.r, rgbColour.g, rgbColour.b);
         }
 
         ctx.moveTo(this.points[0].x, this.points[0].y);
