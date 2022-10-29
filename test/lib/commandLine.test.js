240fb40e78b9b989a4f3944ecce08878c4cdee50
Daniel Wood <s.d.wood.82@googlemail.com>
2022-10-29 16:16:07 +0100

    Update commandLine.test.js

--- test/lib/commandLine.test.js
+++ test/lib/commandLine.test.js
@@ -1,17 +1,87 @@
-
-import {CommandLine} from '../../core/lib/commandLine.js';
 import {Core} from '../../core/core.js';
 
 const core = new Core();
-const commandline = new CommandLine(core);
+const commandline = core.commandLine; // new CommandLine(core);
 
-test('Send keypress to the commandline', () => {
+test('Test Commandline.resetPrompt', () => {
   commandline.handleKeys('L');
-  expect(commandline.command).toBe('L');
+  expect(commandline.command).not.toBe('');
+  commandline.resetPrompt();
+  expect(commandline.command).toBe('');
 });
 
-test.todo('Set update function');
+test('Test Commandline.setPrompt', () => {
+  const testText = 'TestTest';
+  commandline.setPrompt(testText);
+  expect(commandline.prompt).toBe(testText);
+});
 
-test.todo('Test clear prompt function');
+test('Test Commandline.update', () => {
+  const commandTestText = 'commandTestTest';
+  const promptTestText = 'promptTestTest';
 
-test.todo('Test reset prompt function');
+  commandline.command = commandTestText;
+  commandline.prompt = promptTestText;
+  commandline.update();
+  expect(commandline.cmdLine).toBe(promptTestText + commandTestText);
+  commandline.resetPrompt();
+});
+
+test('Test CommandLine.handleKeys', () => {
+  commandline.handleKeys('L');
+  expect(commandline.command).toBe('L');
+  commandline.resetPrompt();
+});
+
+test('Test CommandLine.backPressed', () => {
+  commandline.handleKeys('1234');
+  expect(commandline.command).toBe('1234');
+  commandline.backPressed();
+  expect(commandline.command).toBe('123');
+  commandline.resetPrompt();
+});
+
+test('Test CommandLine.enterPressed', () => {
+  commandline.handleKeys('L');
+  commandline.enterPressed();
+  expect(commandline.lastCommand.length).toBe(1);
+  commandline.resetPrompt();
+  commandline.lastCommand = [];
+});
+
+test('Test CommandLine.addToCommandHistory', () => {
+  commandline.addToCommandHistory('Test');
+  /* commandline should only store unique values */
+  commandline.addToCommandHistory('Test');
+  expect(commandline.lastCommand.length).toBe(1);
+
+  /* commandline should only store last 10 commands */
+  for (let index = 0; index < 20; index++) {
+    commandline.addToCommandHistory(index);
+  }
+
+  expect(commandline.lastCommand.length).toBe(10);
+  commandline.lastCommand = [];
+});
+
+test('Test CommandLine.addToCommandHistory', () => {
+  commandline.addToCommandHistory('3');
+  commandline.addToCommandHistory('2');
+  commandline.addToCommandHistory('1');
+
+  commandline.previousCommand('up');
+  expect(commandline.command).toBe('1');
+  commandline.previousCommand('up');
+  expect(commandline.command).toBe('2');
+  commandline.previousCommand('up');
+  expect(commandline.command).toBe('3');
+  commandline.previousCommand('down');
+  expect(commandline.command).toBe('2');
+  commandline.previousCommand('down');
+  expect(commandline.command).toBe('1');
+  commandline.previousCommand('down');
+  expect(commandline.command).toBe('');
+  commandline.lastCommand = [];
+});
+
+
