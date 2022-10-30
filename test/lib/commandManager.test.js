bff75db09cfa3cedd32d8288f72514ff9ced2d68
Daniel Wood <s.d.wood.82@googlemail.com>
2022-10-30 11:39:26 +0000

    Update commandManager.test.js

--- test/lib/commandManager.test.js
+++ test/lib/commandManager.test.js
@@ -1,8 +1,30 @@
 
-// import {CommandManager} from '../../lib/commandManager.js';
-// import {Core} from '../../core.js';
+import {Core} from '../../core/core.js';
+import {CommandManager} from '../../core/lib/commandManager.js';
+import {Point} from '../../core/entities/point.js';
 
-// const core = new Core();
-// const commandManager = new CommandManager(core);
+const core = new Core();
+const commandManager = new CommandManager(core);
 
-test.todo('Test command manager');
+test('Test CommandManager.createNew', () => {
+  const startPoint = new Point();
+  const endPoint = new Point(10, 10);
+  const points = [startPoint, endPoint];
+
+  const line = commandManager.createNew('Line', {points: points});
+  expect(typeof line).toBe('object');
+
+  const test = commandManager.createNew('test', {points: points});
+  expect(typeof test).toBe('undefined');
+});
+
+test('Test CommandManager.isCommand', () => {
+  expect(commandManager.isCommand('Line')).toBe(true);
+  expect(commandManager.isCommand('Test')).toBe(false);
+});
+
+test('Test CommandManager.getCommandFromShortcut', () => {
+  expect(commandManager.getCommandFromShortcut('L')).toBe('Line');
+  expect(commandManager.getCommandFromShortcut('Test')).toBe('Test');
+});
+
