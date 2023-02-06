cf61de1b1b4fb852fc3cf55f6d7d3fad25c50da8
Daniel Wood <s.d.wood.82@googlemail.com>
2023-02-06 18:36:09 +0000

    make the DXF class an instantiated object.

--- core/lib/fileio.js
+++ core/lib/fileio.js
@@ -2,15 +2,17 @@ import {DXF} from './dxf.js';
 
 export class FileIO {
   static saveDxf(core) {
-    const data = DXF.writeDXF(core);
+    const dxfWriter = new DXF();
+    const data = dxfWriter.writeDXF(core);
     return data;
   }
 
   static openFile(core, data) {
-    DXF.readDxf(core, data);
     core.scene.linkBlockData();
     core.layerManager.checkLayers();
     core.canvas.requestPaint();
     // notify("File Opened")
+      const dxfReader = new DXF();
+      dxfReader.readDxf(core, data);
   }
 }
