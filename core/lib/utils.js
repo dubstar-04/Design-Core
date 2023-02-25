26efe97cd5b5b376abd88eac36adbbd283adf8c2
Daniel Wood <s.d.wood.82@googlemail.com>
2023-02-25 13:23:13 +0000

    Add function to calculate the levenshtein edit distance

--- core/lib/utils.js
+++ core/lib/utils.js
@@ -33,4 +33,33 @@ export class Utils {
 
     return clone;
   }
+
+
+  /**
+   * Returns a levenshtein edit distance to the input strings
+   * The distance represents the minimum number of character edits required to change one string into the other.
+   * a lower number suggests the strings are more similar and value of 0 means they are the same.
+   * @param {string} a
+   * @param {string} b
+   * @returns {integer} levenshtein edit distance
+   * inspired by https://gist.github.com/andrei-m/982927
+   */
+  static getLevenshteinDistance(a, b) {
+    if (!a || !b) return (a || b).length;
+    const m = [];
+    for (let i = 0; i <= b.length; i++) {
+      m[i] = [i];
+      if (i === 0) continue;
+      for (let j = 0; j <= a.length; j++) {
+        m[0][j] = j;
+        if (j === 0) continue;
+        m[i][j] = b.charAt(i - 1) == a.charAt(j - 1) ? m[i - 1][j - 1] : Math.min(
+            m[i-1][j-1] + 1,
+            m[i][j-1] + 1,
+            m[i-1][j] + 1,
+        );
+      }
+    }
+    return m[b.length][a.length];
+  };
 }
