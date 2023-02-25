b0f2a4261e7fe851edc002eb177ed9eafacb7976
Daniel Wood <s.d.wood.82@googlemail.com>
2023-02-25 13:28:17 +0000

    ensure input is valid

--- core/lib/designEngine.js
+++ core/lib/designEngine.js
@@ -115,8 +115,9 @@ export class DesignEngine {
     if (typeof this.core.scene.activeCommand !== 'undefined') {
       this.core.scene.inputArray.push(inputData);
       this.actionInput();
-    } else if (this.core.commandManager.isCommand(this.core.commandManager.getCommandFromShortcut(input))) {
-      this.initialiseItem(this.core.commandManager.getCommandFromShortcut(input));
+    } else if (this.core.commandManager.isCommandOrShortcut(input)) {
+      this.initialiseItem(this.core.commandManager.getCommand(input));
+
       if (this.core.scene.activeCommand.family === 'Tools' && this.core.scene.selectionSet.length || this.core.scene.activeCommand.selectionRequired === false) {
         if (this.core.scene.activeCommand.selectionRequired) {
           this.core.scene.inputArray.push(this.core.scene.selectionSet);
