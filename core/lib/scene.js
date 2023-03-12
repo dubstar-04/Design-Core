61136640a6d751c6cebbd254bc2d1dcc2d5e3dff
Daniel Wood <s.d.wood.82@googlemail.com>
2023-03-12 21:00:44 +0000

    check for valid types

--- core/lib/scene.js
+++ core/lib/scene.js
@@ -155,11 +155,6 @@ export class Scene {
   */
 
   addToScene(type, data, end, index) {
-    // check type is a valid command
-    if (!this.core.commandManager.isCommand(type)) {
-      return;
-    }
-
     if (!data) {
       const colour = 'BYLAYER';
       data = {
@@ -175,6 +170,12 @@ export class Scene {
       // TODO: find a way to create a new type without window
       item = this.core.commandManager.createNew(this.activeCommand.type, data);
     } else {
+      // check type is a valid command
+      if (!this.core.commandManager.isCommand(type)) {
+        console.log('UNSUPPORTED TYPE:', type);
+        this.reset();
+        return;
+      }
       // Create a new item, send it the points array
       item = this.core.commandManager.createNew(type, data);
     }
