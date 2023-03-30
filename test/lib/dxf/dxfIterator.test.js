a3366ec97b0af9758f148d0be6012445ebdf633f
Daniel Wood <s.d.wood.82@googlemail.com>
2023-03-30 20:01:51 +0100

    Create dxfIterator.test.js

---
+++ test/lib/dxf/dxfIterator.test.js
@@ -0,0 +1,55 @@
+import {DxfIterator} from '../../../core/lib/dxf/dxfIterator.js';
+
+const iterator = new DxfIterator();
+const string = '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\nEOF';
+iterator.loadFile(string);
+
+test('Test DxfIterator.loadFile', () => {
+  expect(iterator.lines.length).toBe(11);
+});
+
+test('Test DxfIterator.current', () => {
+  const current = iterator.current();
+  expect(current).toBe('1');
+});
+
+test('Test DxfIterator.nextValue', () => {
+  const nextValue = iterator.nextValue();
+  expect(nextValue).toBe('2');
+});
+
+test('Test DxfIterator.prevValue', () => {
+  iterator.currentIndex = 1;
+  const prevValue = iterator.prevValue();
+  expect(prevValue).toBe('1');
+
+  iterator.currentIndex = 0;
+  const undefinedPrevValue = iterator.prevValue();
+  expect(undefinedPrevValue).toBeUndefined();
+});
+
+test('Test DxfIterator.currentPair', () => {
+  const currentPair = iterator.currentPair();
+  expect(currentPair.code).toBe('1');
+  expect(currentPair.value).toBe('2');
+});
+
+test('Test DxfIterator.nextPair', () => {
+  iterator.currentIndex = iterator.lines.length - 1;
+  const undefinedNextPair = iterator.nextPair();
+  expect(undefinedNextPair).toBeUndefined();
+
+  iterator.currentIndex = 0;
+  const nextPair = iterator.nextPair();
+  expect(nextPair.code).toBe('3');
+  expect(nextPair.value).toBe('4');
+});
+
+test('Test DxfIterator.prevPair', () => {
+  const prevPair = iterator.prevPair();
+  expect(prevPair.code).toBe('1');
+  expect(prevPair.value).toBe('2');
+
+  const undefinedPrevPair = iterator.prevPair();
+  expect(undefinedPrevPair).toBeUndefined();
+});
