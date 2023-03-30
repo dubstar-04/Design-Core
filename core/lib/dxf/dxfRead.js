c857faf49e6465d6414f63689ec044bfea118cd7
Daniel Wood <s.d.wood.82@googlemail.com>
2023-03-30 20:03:24 +0100

    clean up debug

--- core/lib/dxf/dxfRead.js
+++ core/lib/dxf/dxfRead.js
@@ -22,28 +22,23 @@ export class DXFReader {
   parseFile() {
     // TODO: check the file contains data and ends with EOF
     while (this.iterator.nextPair().value !== 'EOF') {
-      // const currentValue = this.iterator.current().trim();
       const currentPair = this.iterator.currentPair();
       switch (true) {
         case (currentPair.value === 'HEADER'):
-          log('read:', currentPair);
           this.header.read(this.iterator);
           break;
 
         case (currentPair.value ==='TABLES'):
-          log('read:', currentPair);
           const tables = new Tables();
           this.tables = tables.read(this.iterator);
           break;
 
         case (currentPair.value === 'BLOCKS'):
-          log('read:', currentPair);
           const blocks = new Blocks();
           this.blocks = blocks.read(this.iterator);
           break;
 
         case (currentPair.value ==='ENTITIES'):
-          log('read:', currentPair);
           const entities = new Entities();
           this.entities = entities.read(this.iterator);
           break;
