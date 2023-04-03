31bd1103a7ce43ad0acfe7c76a8b7d0e768d75cd
Daniel Wood <s.d.wood.82@googlemail.com>
2023-04-03 20:34:32 +0100

    store minimal point data

--- core/lib/selectionWindow.js
+++ core/lib/selectionWindow.js
@@ -17,19 +17,10 @@ export class SelectionWindow {
 
         this.points = [];
         const point1 = new Point(data.points[0].x, data.points[0].y);
-        const point2 = new Point(data.points[1].x, data.points[0].y);
-        const point3 = new Point(data.points[1].x, data.points[1].y);
-        const point4 = new Point(data.points[0].x, data.points[1].y);
-        const point5 = new Point(data.points[0].x, data.points[0].y);
+        const point2 = new Point(data.points[1].x, data.points[1].y);
 
         this.points.push(point1);
         this.points.push(point2);
-        this.points.push(point3);
-        this.points.push(point4);
-        this.points.push(point5);
-
-        this.width = data.points[1].x - data.points[0].x;
-        this.height = data.points[1].y - data.points[0].y;
       }
     }
   }
@@ -44,26 +35,28 @@ export class SelectionWindow {
       ctx.lineWidth = this.lineWidth / scale;
       ctx.beginPath();
       ctx.globalAlpha = 0.2;
-      ctx.fillRect(this.points[0].x, this.points[0].y, this.width, this.height);
+      width = this.points[1].x - this.points[0].x;
+      height = this.points[1].y - this.points[0].y;
+      ctx.fillRect(this.points[0].x, this.points[0].y, width, height);
       ctx.globalAlpha = 1.0;
-      drawRect(this.points);
+      this.drawRect(ctx);
       ctx.stroke();
     } catch { // Cairo
       ctx.setLineWidth(this.lineWidth / scale);
       const rgbColour = Colours.hexToScaledRGB(colour);
       ctx.setSourceRGBA(rgbColour.r, rgbColour.g, rgbColour.b, 0.2);
-      drawRect(this.points);
+      this.drawRect(ctx);
       ctx.fillPreserve();
       ctx.setSourceRGB(rgbColour.r, rgbColour.g, rgbColour.b);
       ctx.stroke();
     }
+  }
 
-    function drawRect(points) {
-      ctx.moveTo(points[0].x, points[0].y);
-      ctx.lineTo(points[1].x, points[1].y);
-      ctx.lineTo(points[2].x, points[2].y);
-      ctx.lineTo(points[3].x, points[3].y);
-      ctx.lineTo(points[4].x, points[4].y);
-    }
+  drawRect(ctx) {
+    ctx.moveTo(this.points[0].x, this.points[0].y);
+    ctx.lineTo(this.points[1].x, this.points[0].y);
+    ctx.lineTo(this.points[1].x, this.points[1].y);
+    ctx.lineTo(this.points[0].x, this.points[1].y);
+    ctx.lineTo(this.points[0].x, this.points[0].y);
   }
 }
