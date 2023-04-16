c59b8183925603b7c6e00fa7660f05eed6a38705
Daniel Wood <s.d.wood.82@googlemail.com>
2023-04-16 13:27:14 +0100

    Add parseInput tests

--- test/lib/commandLine.test.js
+++ test/lib/commandLine.test.js
@@ -49,6 +49,89 @@ test('Test CommandLine.enterPressed', () => {
   commandline.lastCommand = [];
 });
 
+test('Test CommandLine.parseInput', () => {
+  // regular point
+  const regPoint1 = commandline.parseInput('101,102');
+  expect(regPoint1.constructor.name).toBe('Point');
+  expect(regPoint1.x).toBe(101);
+  expect(regPoint1.y).toBe(102);
+
+
+  const regPoint2 = commandline.parseInput('-101,-102');
+  expect(regPoint2.constructor.name).toBe('Point');
+  expect(regPoint2.x).toBe(-101);
+  expect(regPoint2.y).toBe(-102);
+
+  const regPoint3 = commandline.parseInput('10.1,10.2');
+  expect(regPoint3.constructor.name).toBe('Point');
+  expect(regPoint3.x).toBe(10.1);
+  expect(regPoint3.y).toBe(10.2);
+
+  // relative point
+  const relPoint1 = commandline.parseInput('@101,102');
+  expect(relPoint1.constructor.name).toBe('Point');
+  expect(relPoint1.x).toBe(101);
+  expect(relPoint1.y).toBe(102);
+
+  core.scene.points.push(relPoint1);
+  const relPoint2 = commandline.parseInput('@101,102');
+  expect(relPoint2.constructor.name).toBe('Point');
+  expect(relPoint2.x).toBe(202);
+  expect(relPoint2.y).toBe(204);
+
+  const relPoint3 = commandline.parseInput('@-101,-102');
+  expect(relPoint3.constructor.name).toBe('Point');
+  expect(relPoint3.x).toBe(0);
+  expect(relPoint3.y).toBe(0);
+
+  // absolute point
+  const absPoint1 = commandline.parseInput('#101,102');
+  expect(absPoint1.constructor.name).toBe('Point');
+  expect(absPoint1.x).toBe(101);
+  expect(absPoint1.y).toBe(102);
+
+  // integer
+  const int1 = commandline.parseInput('101');
+  expect(int1.constructor.name).toBe('Number');
+  expect(int1).toBe(101);
+
+  // negative integer
+  const int2 = commandline.parseInput('-101');
+  expect(int2.constructor.name).toBe('Number');
+  expect(int2).toBe(-101);
+
+  // float
+  const float1 = commandline.parseInput('101.1');
+  expect(float1.constructor.name).toBe('Number');
+  expect(float1).toBe(101.1);
+
+  // negative float
+  const float2 = commandline.parseInput('-101.1');
+  expect(float2.constructor.name).toBe('Number');
+  expect(float2).toBe(-101.1);
+
+  // string
+  const str1 = 'str';
+  const string1 = commandline.parseInput(str1);
+  expect(string1.constructor.name).toBe('String');
+  expect(string1).toBe(str1);
+
+  const str2 = '@str';
+  const string2 = commandline.parseInput(str2);
+  expect(string2.constructor.name).toBe('String');
+  expect(string2).toBe(str2);
+
+  const str3 = 'str3';
+  const string3 = commandline.parseInput(str3);
+  expect(string3.constructor.name).toBe('String');
+  expect(string3).toBe(str3);
+
+  const str4 = 'str-4';
+  const string4 = commandline.parseInput(str4);
+  expect(string4.constructor.name).toBe('String');
+  expect(string4).toBe(str4);
+});
+
 test('Test CommandLine.addToCommandHistory', () => {
   commandline.addToCommandHistory('Test');
   /* commandline should only store unique values */
