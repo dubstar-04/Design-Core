6110c395a4d502f94190659c83beb869442c6610
Daniel Wood <s.d.wood.82@googlemail.com>
2023-04-16 10:01:23 +0100

    Refactor input method

--- core/entities/circle.js
+++ core/entities/circle.js
@@ -33,33 +33,20 @@ export class Circle extends Entity {
     return command;
   }
 
-  prompt(core) {
-    const num = core.scene.inputArray.length;
+  processInput(num, input, inputType, core) {
     const expectedType = [];
-    let reset = false;
-    let action = false;
     const prompt = [];
 
-    expectedType[0] = ['undefined'];
-    prompt[0] = Strings.Input.CENTER;
+    prompt[1] = Strings.Input.CENTER;
+    expectedType[1] = ['Point', 'Number'];
 
-    expectedType[1] = ['object'];
-    prompt[1] = Strings.Input.POINTORRADIUS;
+    prompt[2] = Strings.Input.POINTORRADIUS;
+    expectedType[2] = ['Point', 'Number'];
 
-    expectedType[2] = ['object', 'number'];
-    prompt[2] = prompt[1];
 
-    const validInput = expectedType[num].includes(typeof core.scene.inputArray[num - 1]);
-
-    if (!validInput) {
-      core.scene.inputArray.pop();
-    } else if (core.scene.inputArray.length === this.minPoints) {
-      action = true;
-      reset = true;
-    }
-
-    return {promptInput: prompt[core.scene.inputArray.length], resetBool: reset, actionBool: action, validInput: validInput};
+    return {expectedType: expectedType, prompt: prompt, reset: (num === prompt.length - 1), action: num >= this.minPoints};
   }
+
 
   getRadius() {
     return Utils.distBetweenPoints(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y); ;
