c81ef88808714c0b9e3b5873b9e3967c100cd48f
Daniel Wood <s.d.wood.82@googlemail.com>
2023-04-16 10:02:23 +0100

    Refactor input method

--- core/tools/erase.js
+++ core/tools/erase.js
@@ -1,8 +1,10 @@
+import {Strings} from '../lib/strings.js';
 import {Tool} from './tool.js';
 
 export class Erase extends Tool {
   constructor() {
     super();
+    this.minPoints = 0;
   }
 
   static register() {
@@ -10,32 +12,19 @@ export class Erase extends Tool {
     return command;
   }
 
-  prompt(core) {
-    const num = core.scene.inputArray.length;
+  processInput(num, input, inputType, core) {
     const expectedType = [];
-    let reset = false;
-    let action = false;
     const prompt = [];
 
-    expectedType[0] = ['undefined'];
-    prompt[0] = 'Select Items To ' + this.type;
+    const selection = `${core.scene.selection.selectionSet.length}  ${Strings.Input.SELECTED}`;
+    const noSelection = Strings.Input.SELECTENTITIES;
 
-    expectedType[1] = ['object'];
-    prompt[1] = core.scene.selection.selectionSet.length + ' Item(s) selected: Add more or press Enter to Erase';
+    prompt[1] = core.scene.selection.selectionSet.length ? selection : noSelection;
+    expectedType[1] = ['CanvasSelection', 'SelectionAccepted'];
 
-    expectedType[2] = ['boolean'];
-    prompt[2] = '';
+    const accepted = (inputType === 'SelectionAccepted');
 
-    const validInput = expectedType[num].includes(typeof core.scene.inputArray[num - 1]);
-
-    if (!validInput) {
-      core.scene.inputArray.pop();
-    } else if (core.scene.inputArray.length === 2) {
-      action = true;
-      reset = true;
-    }
-
-    return {promptInput: prompt[core.scene.inputArray.length], resetBool: reset, actionBool: action, validInput: validInput};
+    return {expectedType: expectedType, prompt: prompt, reset: accepted, action: accepted};
   }
 
   action(core) {
