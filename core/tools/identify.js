ca4fc91dbc780c486bf5561398096f340767babc
Daniel Wood <s.d.wood.82@googlemail.com>
2023-04-22 07:12:24 +0100

    Convert Tool to async execution

--- core/tools/identify.js
+++ core/tools/identify.js
@@ -1,11 +1,12 @@
 import {Strings} from '../lib/strings.js';
 import {Tool} from './tool.js';
+import {Input, PromptOptions} from '../lib/inputManager.js';
 
 export class Identify extends Tool {
   constructor() {
     super();
-    this.selectionRequired = false;
-    this.minPoints = 1;
+    // this.selectionRequired = false;
+    // this.minPoints = 1;
   }
 
   static register() {
@@ -13,19 +14,21 @@ export class Identify extends Tool {
     return command;
   }
 
-  processInput(num, input, inputType, core) {
-    const expectedType = [];
-    const prompt = [];
+  async execute(core) {
+    try {
+      const op = new PromptOptions(Strings.Input.START, [Input.Type.POINT]);
+      const pt1 = await core.scene.inputManager.requestInput(op);
+      this.points.push(pt1);
 
-    prompt[1] = Strings.Input.START;
-    expectedType[1] = ['Point'];
-
-    return {expectedType: expectedType, prompt: prompt, reset: (num === prompt.length - 1), action: (num === prompt.length - 1)};
+      core.scene.inputManager.executeCommand();
+    } catch (err) {
+      log(this.type, err);
+    }
   }
 
   action(core) {
-    const x = core.scene.points.at(-1).x.toFixed(1);
-    const y = core.scene.points.at(-1).y.toFixed(1);
+    const x = this.points.at(-1).x.toFixed(1);
+    const y = this.points.at(-1).y.toFixed(1);
     const id = (`X:${x} Y:${y}`);
     core.notify(id);
   }
