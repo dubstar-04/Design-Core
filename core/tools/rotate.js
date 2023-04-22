3401966e289a9525dd1b6031b2b64629bfbc5500
Daniel Wood <s.d.wood.82@googlemail.com>
2023-04-22 07:12:32 +0100

    Convert Tool to async execution

--- core/tools/rotate.js
+++ core/tools/rotate.js
@@ -1,10 +1,13 @@
 import {Strings} from '../lib/strings.js';
 import {Tool} from './tool.js';
+import {Utils} from '../lib/utils.js';
+import {Input, PromptOptions} from '../lib/inputManager.js';
+
 
 export class Rotate extends Tool {
   constructor() {
     super();
-    this.showHelperGeometry = true;
+    // this.showHelperGeometry = true;
   }
 
   static register() {
@@ -12,48 +15,75 @@ export class Rotate extends Tool {
     return command;
   }
 
-  processInput(num, input, inputType, core) {
-    const expectedType = [];
-    const prompt = [];
+  async execute(core) {
+    try {
+      const op = new PromptOptions(Strings.Input.SELECTENTITIES, [Input.Type.SELECTIONSET]);
 
-    const selection = `${core.scene.selection.selectionSet.length}  ${Strings.Input.SELECTED}`;
-    const noSelection = Strings.Input.SELECTENTITIES;
+      if (!core.scene.selectionManager.selectionSet.selectionSet.length) {
+        await core.scene.inputManager.requestInput(op);
+      }
 
-    prompt[1] = core.scene.selection.selectionSet.length ? selection : noSelection;
-    expectedType[1] = ['CanvasSelection', 'SelectionAccepted'];
+      const op2 = new PromptOptions(Strings.Input.BASEPOINT, [Input.Type.POINT]);
+      const pt1 = await core.scene.inputManager.requestInput(op2);
+      this.points.push(pt1);
 
-    prompt[2] = Strings.Input.BASEPOINT;
-    expectedType[2] = ['Point'];
+      const op3 = new PromptOptions(Strings.Input.START, [Input.Type.POINT]);
+      const pt2 = await core.scene.inputManager.requestInput(op3);
+      this.points.push(pt2);
 
-    prompt[3] = Strings.Input.START;
-    expectedType[3] = ['Point'];
+      const op4 = new PromptOptions(Strings.Input.END, [Input.Type.POINT, Input.Type.NUMBER]);
+      const pt3 = await core.scene.inputManager.requestInput(op4);
+      if (Input.getType(pt3) === Input.Type.POINT) {
+        this.points.push(pt3);
+      } else if (Input.getType(pt3) === Input.Type.NUMBER) {
+        const basePoint = this.points.at(0);
+        const startPoint = this.points.at(1);
+        const angle = Utils.degrees2radians(pt3);
+        const point = startPoint.rotate(basePoint, angle);
+        this.points.push(point);
+      }
 
-    prompt[4] = Strings.Input.END;
-    expectedType[4] = ['Point'];
 
-    return {expectedType: expectedType, prompt: prompt, reset: (num === prompt.length - 1), action: (num === prompt.length - 1)};
+      core.scene.inputManager.executeCommand();
+    } catch (err) {
+      log(this.type, err);
+    }
   }
 
   preview(core) {
-    if (core.scene.tempPoints.length > 2) {
-      const A = core.scene.tempPoints[0].x - core.scene.tempPoints[1].x;
-      const O = core.scene.tempPoints[0].y - core.scene.tempPoints[1].y;
+    const mousePoint = core.mouse.pointOnScene();
 
-      const A1 = core.scene.tempPoints[0].x - core.scene.tempPoints[2].x;
-      const O1 = core.scene.tempPoints[0].y - core.scene.tempPoints[2].y;
+    if (this.points.length >= 1) {
+      // Draw a line
+      const points = [this.points.at(0), mousePoint];
+
+      const data = {
+        points: points,
+        colour: core.settings.helpergeometrycolour.toString(),
+      };
+
+      core.scene.addToTempItems('Line', data);
+    }
+
+    if (this.points.length >= 2) {
+      const A = this.points[0].x - this.points[1].x;
+      const O = this.points[0].y - this.points[1].y;
+
+      const A1 = this.points[0].x - mousePoint.x;
+      const O1 = this.points[0].y - mousePoint.y;
 
       const ang1 = Math.atan2(O, A);
       const ang2 = Math.atan2(O1, A1);
 
       const theta = ang2 - ang1;
 
-      for (let i = 0; i < core.scene.selection.selectionSet.length; i++) {
-        for (let j = 0; j < core.scene.selection.selectedItems[i].points.length; j++) {
-          const x = core.scene.tempPoints[0].x + (core.scene.items[core.scene.selection.selectionSet[i]].points[j].x - core.scene.tempPoints[0].x) * Math.cos(theta) - (core.scene.items[core.scene.selection.selectionSet[i]].points[j].y - core.scene.tempPoints[0].y) * Math.sin(theta);
-          const y = core.scene.tempPoints[0].y + (core.scene.items[core.scene.selection.selectionSet[i]].points[j].x - core.scene.tempPoints[0].x) * Math.sin(theta) + (core.scene.items[core.scene.selection.selectionSet[i]].points[j].y - core.scene.tempPoints[0].y) * Math.cos(theta);
+      for (let i = 0; i < core.scene.selectionManager.selectionSet.selectionSet.length; i++) {
+        for (let j = 0; j < core.scene.selectionManager.selectedItems[i].points.length; j++) {
+          const x = this.points[0].x + (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].x - this.points[0].x) * Math.cos(theta) - (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].y - this.points[0].y) * Math.sin(theta);
+          const y = this.points[0].y + (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].x - this.points[0].x) * Math.sin(theta) + (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].y - this.points[0].y) * Math.cos(theta);
 
-          core.scene.selection.selectedItems[i].points[j].x = x;
-          core.scene.selection.selectedItems[i].points[j].y = y;
+          core.scene.selectionManager.selectedItems[i].points[j].x = x;
+          core.scene.selectionManager.selectedItems[i].points[j].y = y;
         }
       }
     }
@@ -61,24 +91,24 @@ export class Rotate extends Tool {
 
 
   action(core) {
-    const A = core.scene.points[0].x - core.scene.points[1].x;
-    const O = core.scene.points[0].y - core.scene.points[1].y;
+    const A = this.points[0].x - this.points[1].x;
+    const O = this.points[0].y - this.points[1].y;
 
-    const A1 = core.scene.points[0].x - core.scene.points[2].x;
-    const O1 = core.scene.points[0].y - core.scene.points[2].y;
+    const A1 = this.points[0].x - this.points[2].x;
+    const O1 = this.points[0].y - this.points[2].y;
 
     const ang1 = Math.atan2(O, A);
     const ang2 = Math.atan2(O1, A1);
 
     const theta = ang2 - ang1;
 
-    for (let i = 0; i < core.scene.selection.selectionSet.length; i++) {
-      for (let j = 0; j < core.scene.selection.selectedItems[i].points.length; j++) {
-        const x = core.scene.points[0].x + (core.scene.items[core.scene.selection.selectionSet[i]].points[j].x - core.scene.points[0].x) * Math.cos(theta) - (core.scene.items[core.scene.selection.selectionSet[i]].points[j].y - core.scene.points[0].y) * Math.sin(theta);
-        const y = core.scene.points[0].y + (core.scene.items[core.scene.selection.selectionSet[i]].points[j].x - core.scene.points[0].x) * Math.sin(theta) + (core.scene.items[core.scene.selection.selectionSet[i]].points[j].y - core.scene.points[0].y) * Math.cos(theta);
+    for (let i = 0; i < core.scene.selectionManager.selectionSet.selectionSet.length; i++) {
+      for (let j = 0; j < core.scene.selectionManager.selectedItems[i].points.length; j++) {
+        const x = this.points[0].x + (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].x - this.points[0].x) * Math.cos(theta) - (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].y - this.points[0].y) * Math.sin(theta);
+        const y = this.points[0].y + (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].x - this.points[0].x) * Math.sin(theta) + (core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].y - this.points[0].y) * Math.cos(theta);
 
-        core.scene.items[core.scene.selection.selectionSet[i]].points[j].x = x;
-        core.scene.items[core.scene.selection.selectionSet[i]].points[j].y = y;
+        core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].x = x;
+        core.scene.items[core.scene.selectionManager.selectionSet.selectionSet[i]].points[j].y = y;
       }
     }
   };
