ab47e3d2e8a2af09789fe08285638b8a586654e6
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-08 14:30:23 +0100

    Create distance.test.js

---
+++ test/tools/distance.test.js
@@ -0,0 +1,29 @@
+import {Core} from '../../core/core.js';
+import {Point} from '../../core/entities/point.js';
+import {Distance} from '../../core/tools/distance.js';
+
+const core = new Core();
+let output;
+
+core.notify = (notification) => {
+  output = notification;
+};
+
+test('Test distance.action', () => {
+  // Add items to scene
+  core.scene.addToScene('Line', {points: [new Point(), new Point(0, 10)]});
+
+  const distance = new Distance();
+
+  // set base point
+  distance.points.push(new Point());
+
+  // set destination point
+  distance.points.push(new Point(0, 10));
+
+  // Perform distance
+  distance.action(core);
+
+  expect(output).not.toBeUndefined();
+  expect(output).toBe('Length: 10.0 &#916;X: 0.0 &#916;Y: 10.0');
+});
