c9c39a1295214ebf3a776c71320768b238ead2b8
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-10 21:22:18 +0100

    Add R2000 specific groupcodes

--- core/styles/styleManager.js
+++ core/styles/styleManager.js
@@ -1,4 +1,6 @@
+import {DXFFile} from '../lib/dxf/dxfFile.js';
 import {Style} from './style.js';
+
 
 export class StyleManager {
   constructor(core) {
@@ -140,6 +142,8 @@ export class StyleManager {
     // Create table data for text styles
     file.writeGroupCode('0', 'TABLE');
     file.writeGroupCode('2', 'STYLE');
+    file.writeGroupCode('5', file.nextHandle(), DXFFile.Version.R2000); // Handle
+    file.writeGroupCode('100', 'AcDbSymbolTable', DXFFile.Version.R2000);
     file.writeGroupCode('70', this.styleCount());
 
     for (let i = 0; i < this.styleCount(); i++) {
