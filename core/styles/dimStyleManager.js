020d8becf0cc0672ec703def96d040b6172c3e2e
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-14 11:33:46 +0100

    use style manager base

--- core/styles/dimStyleManager.js
+++ core/styles/dimStyleManager.js
@@ -1,140 +1,14 @@
 import {DXFFile} from '../lib/dxf/dxfFile.js';
-import {Logging} from '../lib/logging.js';
 import {DimStyle} from './dimStyle.js';
+import {StyleManagerBase} from './styleManagerBase.js';
 
-export class DimStyleManager {
+export class DimStyleManager extends StyleManagerBase {
   constructor(core) {
-    // TODO: Can dimstylemanager and style manager be merged?
-
-    this.styles = [];
-    this.currentstyle = 'STANDARD';
-    this.core = core;
-    this.addStandardStyles();
+    super(core);
   }
 
-  getStyles() {
-    return this.styles;
-  }
-
-  styleCount() {
-    return this.styles.length;
-  }
-
-  newStyle() {
-    this.addStyle({
-      'name': this.getUniqueName('NEW_STYLE'),
-    });
-  }
-
-  getUniqueName(name) {
-    let count = 0;
-    let styStr = name.replace(/ /g, '_').toUpperCase();
-    for (let i = 0; i < this.styleCount(); i++) {
-      if (this.styles[i].name.includes(styStr)) {
-        count = count + 1;
-      }
-    }
-    if (count > 0) {
-      styStr = styStr + '_' + count;
-    }
-
-    return styStr;
-  }
-
-  addStyle(style) {
-    const newstyle = new DimStyle(style);
-    if (!this.styleExists(style)) {
-      this.styles.push(newstyle);
-      this.core.scene.saveRequired();
-    }
-  }
-
-  deleteStyle(styleIndex) {
-    const styleToDelete = this.getStyleByIndex(styleIndex).name;
-
-    if (styleToDelete.toUpperCase() === 'STANDARD') {
-      Logging.instance.warn('STANDARD style cannot be deleted');
-      return;
-    }
-
-    const selectionSet = [];
-
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].style === styleToDelete) {
-        selectionSet.push(i);
-      }
-    }
-
-    selectionSet.sort();
-    for (let j = 0; j < selectionSet.length; j++) {
-      items.splice((selectionSet[j] - j), 1);
-    }
-
-    // Delete The style
-    this.styles.splice(styleIndex, 1);
-  }
-
-  getCstyle() {
-    return this.currentstyle;
-  }
-
-  setCstyle(cstyle) {
-    this.currentstyle = cstyle;
-  }
-
-  styleExists(style) {
-    let i = this.styleCount();
-    while (i--) {
-      if (this.styles[i].name === style.name) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  checkStyles() {
-    if (!this.styleCount()) {
-      this.addStandardStyles();
-    }
-
-    for (let i = 0; i < items.length; i++) {
-      const style = (items[i].style);
-      this.addstyle({
-        'name': style,
-      });
-    }
-  }
-
-  addStandardStyles() {
-    this.addStyle({
-      'name': 'STANDARD',
-    });
-    this.core.scene.saveRequired();
-  }
-
-  getStyleByName(styleName) {
-    for (let i = 0; i < this.styleCount(); i++) {
-      if (this.styles[i].name === styleName) {
-        return this.styles[i];
-      }
-    }
-  }
-
-  getStyleByIndex(styleIndex) {
-    return this.styles[styleIndex];
-  }
-
-
-  renameStyle(styleIndex, newName) {
-    const newUniqueName = this.getUniqueName(newName);
-
-    if (this.getStyleByIndex(styleIndex).name.toUpperCase() !== 'STANDARD') {
-      if (this.getStyleByIndex(styleIndex).name === this.getCStyle()) {
-        this.setCStyle(newUniqueName);
-      }
-
-      this.styles[styleIndex].name = newUniqueName;
-    }
+  createStyle(style) {
+    return new DimStyle(style);
   }
 
   dxf(file) {
--- core/styles/styleManager.js
+++ core/styles/styleManager.js
@@ -1,141 +1,14 @@
 import {DXFFile} from '../lib/dxf/dxfFile.js';
+import {StyleManagerBase} from './styleManagerBase.js';
 import {Style} from './style.js';
 
-
-export class StyleManager {
+export class StyleManager extends StyleManagerBase {
   constructor(core) {
-    this.styles = [];
-    this.currentstyle = 'STANDARD';
-    this.core = core;
-    this.addStandardStyles();
+    super(core);
   }
 
-
-  getStyles() {
-    return this.styles;
-  }
-
-  styleCount() {
-    return this.styles.length;
-  }
-
-  newStyle() {
-    this.addStyle({
-      'name': this.getUniqueName('NEW_STYLE'),
-    });
-  }
-
-  getUniqueName(name) {
-    let count = 0;
-    let styStr = name.replace(/ /g, '_').toUpperCase();
-    for (let i = 0; i < this.styleCount(); i++) {
-      if (this.styles[i].name.includes(styStr)) {
-        count = count + 1;
-      }
-    }
-    if (count > 0) {
-      styStr = styStr + '_' + count;
-    }
-
-    return styStr;
-  };
-
-  addStyle(style) {
-    const newstyle = new Style(style);
-    if (!this.styleExists(style)) {
-      this.styles.push(newstyle);
-      this.core.scene.saveRequired();
-    }
-  }
-
-  deleteStyle(styleIndex) {
-    const styleToDelete = this.getStyleByIndex(styleIndex).name;
-
-    if (styleToDelete.toUpperCase() === 'STANDARD') {
-      return;
-    }
-
-    const selectionSet = [];
-
-    for (let i = 0; i < this.core.scene.items.length; i++) {
-      if (items[i].style === styleToDelete) {
-        selectionSet.push(i);
-      }
-    }
-
-    selectionSet.sort();
-    for (let j = 0; j < selectionSet.length; j++) {
-      items.splice((selectionSet[j] - j), 1);
-    }
-
-    // Delete The style
-    this.styles.splice(styleIndex, 1);
-  }
-
-  getCstyle() {
-    return this.currentstyle;
-  }
-
-  setCstyle(cstyle) {
-    this.currentstyle = cstyle;
-  }
-
-  styleExists(style) {
-    let i = this.styleCount();
-    while (i--) {
-      if (this.styles[i].name === style.name) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  checkStyles() {
-    if (!this.styleCount()) {
-      this.addStandardStyles();
-    }
-
-    for (let i = 0; i < this.core.scene.items.length; i++) {
-      const style = (items[i].style);
-      this.addstyle({
-        'name': style,
-      });
-    }
-  }
-
-  addStandardStyles() {
-    this.addStyle({
-      'name': 'STANDARD',
-    });
-    this.addStyle({
-      'name': 'ANNOTATIVE',
-    });
-    this.core.scene.saveRequired();
-  }
-
-  getStyleByName(styleName) {
-    for (let i = 0; i < this.styleCount(); i++) {
-      if (this.styles[i].name === styleName) {
-        return this.styles[i];
-      }
-    }
-  }
-
-  getStyleByIndex(styleIndex) {
-    return this.styles[styleIndex];
-  }
-
-
-  renameStyle(styleIndex, newName) {
-    const newUniqueName = this.getUniqueName(newName);
-
-    if (this.getStyleByIndex(styleIndex).name.toUpperCase() !== 'STANDARD') {
-      if (this.getStyleByIndex(styleIndex).name === this.getCStyle()) {
-        this.setCStyle(newUniqueName);
-      }
-
-      this.styles[styleIndex].name = newUniqueName;
-    }
+  createStyle(style) {
+    return new Style(style);
   }
 
   dxf(file) {
