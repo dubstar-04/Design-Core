08fb7b8c5b2e8a109314d02b6fae7da08741d088
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-16 20:31:14 +0100

    Create ltype.js

---
+++ core/styles/ltype.js
@@ -0,0 +1,84 @@
+import {DXFFile} from '../lib/dxf/dxfFile.js';
+
+export class LType {
+  constructor(data) {
+    this.type = 'LType';
+    this.name = '';
+    this.description = '';
+    this.pattern = [];
+    this.flags = 0;
+
+    if (data) {
+      if (data.name || data[2]) {
+        // DXF Groupcode 2 - ltype name
+        this.name = data.name || data[2];
+      }
+
+      if (data.description || data[3]) {
+        // DXF Groupcode 3 - description text
+        this.description = data.description || data[3];
+      }
+
+      if (data.pattern || data[49]) {
+        // DXF Groupcode 49 - line type pattern
+        this.pattern = data.pattern || data[49];
+      }
+
+      if (data[73]) {
+        // DXF Groupcode 73 - number of line type elements in data[49]
+        if (data[73] !== this.pattern.length) {
+          throw Error('invalid line type pattern');
+        }
+      }
+
+      if (data.flags || data[70]) {
+        // DXF Groupcode 70 - Polyline flag (bit-coded; default = 0):
+        // 16 = If set, table entry is externally dependent on an xref
+        // 32 = If this bit and bit 16 are both set, the externally dependent xref has been successfully resolved
+        // 64 = line type was referenced by at least one entity in the drawing the last time the drawing was edited. (This flag can be ignored by most programs)
+        this.flags = data.flags || data[70];
+      }
+    }
+  }
+
+  /**
+   * Get a dash pattern that cairo and html canvas understand
+   * @param {array} scale
+   * @returns array for dash pattern
+   */
+  getPattern(scale) {
+    // DXF Patterns use -ve values for gaps and 0.0 for dots
+    // cairo and canvas require pattern to include +ve non-zero values
+    const pattern = this.pattern.map((x) => (Math.abs(x) + 1) / scale);
+    return pattern;
+  }
+
+  /**
+   * Write the dxf representation to file
+   * @param {DXFFile} file
+   */
+  dxf(file) {
+    file.writeGroupCode('0', 'LTYPE');
+    file.writeGroupCode('5', file.nextHandle(), DXFFile.Version.R2000);
+    file.writeGroupCode('100', 'AcDbSymbolTableRecord', DXFFile.Version.R2000);
+    file.writeGroupCode('100', 'AcDbLinetypeTableRecord', DXFFile.Version.R2000);
+    file.writeGroupCode('2', this.name);
+    file.writeGroupCode('70', this.flags);
+    file.writeGroupCode('3', this.description);
+
+    if (this.pattern.length) {
+      file.writeGroupCode('73', this.pattern.length);
+
+      const patternLength = this.pattern.reduce(
+          (accumulator, patternValue) => accumulator + Math.abs(patternValue),
+      );
+
+      file.writeGroupCode('40', patternLength);
+
+      for (let i = 0; i < this.pattern.length; i++) {
+        file.writeGroupCode('49', this.pattern[i]);
+        file.writeGroupCode('74', '0');
+      }
+    }
+  }
+}
