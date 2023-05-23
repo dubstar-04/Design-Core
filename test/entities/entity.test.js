a0d385e4f192c3c0d85122ef7dadeaf3a4740161
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:28:22 +0100

    Create entity.test.js

---
+++ test/entities/entity.test.js
@@ -0,0 +1,6 @@
+
+
+import {Core} from '../../core/core.js';
+import {Entity} from '../../core/entities/entity.js';
+import {Line} from '../../core/entities/line.js';
+import {Point} from '../../core/entities/point.js';
