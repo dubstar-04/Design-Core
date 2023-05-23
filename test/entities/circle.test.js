779deb203c2247261425120aa88810c88cae0f1a
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:27:23 +0100

    Add getRadius Test

--- test/entities/circle.test.js
+++ test/entities/circle.test.js
@@ -1,3 +1,11 @@
+import {Circle} from '../../core/entities/circle.js';
+import {Point} from '../../core/entities/point.js';
+
+
+test('Test Circle.getRadius', () => {
+  const circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
+  expect(circle.getRadius()).toBe(100);
+});
 test('Test Circle.boundingBox', () => {
   let circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
   expect(circle.boundingBox().xMin).toBeCloseTo(0);
