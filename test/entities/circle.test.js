0ce50a6e9a3ab79b3de2c8582340bb5ab77ae258
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:27:34 +0100

    Add setRadius Test

--- test/entities/circle.test.js
+++ test/entities/circle.test.js
@@ -6,6 +6,15 @@ test('Test Circle.getRadius', () => {
   const circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
   expect(circle.getRadius()).toBe(100);
 });
+
+test('Test Circle.setRadius', () => {
+  // create a circle with radius 100
+  const circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
+  // set radius to 200
+  circle.setRadius(200);
+  expect(circle.getRadius()).toBe(200);
+  expect(circle.points[1].x).toBe(300);
+});
 test('Test Circle.boundingBox', () => {
   let circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
   expect(circle.boundingBox().xMin).toBeCloseTo(0);
