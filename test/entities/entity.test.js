ec8d14ec2877c1bdeb95328c46f5e6f76bfaf0a5
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:28:40 +0100

    test getColour

--- test/entities/entity.test.js
+++ test/entities/entity.test.js
@@ -4,3 +4,10 @@ import {Core} from '../../core/core.js';
 import {Entity} from '../../core/entities/entity.js';
 import {Line} from '../../core/entities/line.js';
 import {Point} from '../../core/entities/point.js';
+
+const core = new Core();
+
+test('Test Entity.getColour', () => {
+  const entity = new Entity();
+  expect(entity.getColour(core)).toBe('#00BFFF');
+});
