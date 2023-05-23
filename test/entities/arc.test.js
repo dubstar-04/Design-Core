4030c4827a2b6d185801987a84a7026b93e6f0fe
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:24:36 +0100

    Add boundingbox test

--- test/entities/arc.test.js
+++ test/entities/arc.test.js
@@ -27,3 +27,18 @@ test('Test Arc.execute', () => {
   // need to await enter pressed or similar without affecting user experience
   // expect(core.scene.items.length).toBe(1),
 });
+test('Test Arc.boundingBox', () => {
+  // clockwise 45 degrees 45 - 0
+  let arc = new Arc({points: [new Point(100, 100), new Point(200, 100), new Point(170.71, 170.71)]});
+  expect(arc.boundingBox().xMin).toBeCloseTo(170.71);
+  expect(arc.boundingBox().xMax).toBeCloseTo(200);
+  expect(arc.boundingBox().yMin).toBeCloseTo(100);
+  expect(arc.boundingBox().yMax).toBeCloseTo(170.71);
+
+  // anticlockwise 45 degrees: 0 - 45
+  arc = new Arc({points: [new Point(100, 100), new Point(200, 100), new Point(170.71, 170.71)], direction: -1});
+  expect(arc.boundingBox().xMin).toBeCloseTo(0);
+  expect(arc.boundingBox().xMax).toBeCloseTo(200);
+  expect(arc.boundingBox().yMin).toBeCloseTo(0);
+  expect(arc.boundingBox().yMax).toBeCloseTo(200);
+});
