fb8838f0405316771442da7858cd06b06cee83bf
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:25:06 +0100

    Add boundingbox test

---
+++ test/entities/circle.test.js
@@ -0,0 +1,13 @@
+test('Test Circle.boundingBox', () => {
+  let circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
+  expect(circle.boundingBox().xMin).toBeCloseTo(0);
+  expect(circle.boundingBox().xMax).toBeCloseTo(200);
+  expect(circle.boundingBox().yMin).toBeCloseTo(0);
+  expect(circle.boundingBox().yMax).toBeCloseTo(200);
+
+  circle = new Circle({points: [new Point(0, 0)], radius: 100});
+  expect(circle.boundingBox().xMin).toBeCloseTo(-100);
+  expect(circle.boundingBox().xMax).toBeCloseTo(100);
+  expect(circle.boundingBox().yMin).toBeCloseTo(-100);
+  expect(circle.boundingBox().yMax).toBeCloseTo(100);
+});
