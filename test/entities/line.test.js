1bd742656cb6c8390b939a8bd1d71478567d77c4
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 13:12:14 +0100

    test closestPoint

--- test/entities/line.test.js
+++ test/entities/line.test.js
@@ -1,6 +1,16 @@
 import {Line} from '../../core/entities/line.js';
 import {Point} from '../../core/entities/point.js';
 
+test('Test BasePolyline.closestPoint', () => {
+  const points = [new Point(100, 100), new Point(200, 100)];
+  const line = new Line({points: points});
+  // line segment
+  const point1 = new Point(150, 85);
+  const closest1 = line.closestPoint(point1);
+  expect(closest1[0].x).toBeCloseTo(150);
+  expect(closest1[0].y).toBeCloseTo(100);
+  expect(closest1[1]).toBeCloseTo(15);
+});
 
 test('Test Line.boundingBox', () => {
   let line = new Line({points: [new Point(101, 102), new Point(201, 202)]});
