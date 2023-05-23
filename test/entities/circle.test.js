007bfeccdca37a714b5f2f604283adbbd08c459d
Daniel Wood <s.d.wood.82@googlemail.com>
2023-05-23 12:27:50 +0100

    add closestPoint test

--- test/entities/circle.test.js
+++ test/entities/circle.test.js
@@ -15,6 +15,25 @@ test('Test Circle.setRadius', () => {
   expect(circle.getRadius()).toBe(200);
   expect(circle.points[1].x).toBe(300);
 });
+
+test('Test Circle.closestPoint', () => {
+  // create a circle with radius 100
+  const circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
+  // inside
+  const point1 = new Point(150, 100);
+  const closest1 = circle.closestPoint(point1);
+  expect(closest1[0].x).toBe(200);
+  expect(closest1[0].y).toBe(100);
+  expect(closest1[1]).toBe(50);
+
+  // outside
+  const point2 = new Point(250, 100);
+  const closest2 = circle.closestPoint(point2);
+  expect(closest2[0].x).toBe(200);
+  expect(closest2[0].y).toBe(100);
+  expect(closest2[1]).toBe(50);
+});
+
 test('Test Circle.boundingBox', () => {
   let circle = new Circle({points: [new Point(100, 100), new Point(200, 100)]});
   expect(circle.boundingBox().xMin).toBeCloseTo(0);
