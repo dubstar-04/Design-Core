74f2fa5357539007b27b9e1ffd39490f4466e9c8
Daniel Wood <s.d.wood.82@googlemail.com>
2023-12-13 20:37:50 +0000

    format comments

--- core/entities/dimension.js
+++ core/entities/dimension.js
@@ -116,9 +116,16 @@ export class Dimension extends Entity {
       if (data.dimType || data[70]) {
         // DXF Groupcode 70 - Dimension Type
         // Values 0-6 are integer values that represent the dimension type. Values 32, 64, and 128
-        // are bit values, which are added to the integer values (value 32 is always set in R13 and later releases)
-        // 0 = Rotated, horizontal, or vertical; 1 = Aligned; 2 = Angular; 3 = Diameter; 4 = Radius; 5 = Angular 3 point;
-        // 6 = Ordinate; 32 = Indicates that the block reference (group code 2) is referenced by this dimension only
+        // are bit values, which are added to the integer values
+        // (value 32 is always set in R13 and later releases)
+        // 0 = Rotated, horizontal, or vertical;
+        // 1 = Aligned;
+        // 2 = Angular;
+        // 3 = Diameter;
+        // 4 = Radius;
+        // 5 = Angular 3 point;
+        // 6 = Ordinate;
+        // 32 = Indicates that the block reference (group code 2) is referenced by this dimension only
         // 64 = Ordinate type. This is a bit value (bit 7) used only with integer value 6.
         // If set, ordinate is X-type; if not set, ordinate is Y-type
         // 128 = This is a bit value (bit 8) added to the other group 70 values if the dimension text
