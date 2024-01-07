662c9cd8e072623f838e2d7f516122f5b38b3f5b
Daniel Wood <s.d.wood.82@googlemail.com>
2024-01-07 11:30:03 +0000

    Create ltypeManager.test.js

---
+++ test/styles/ltypeManager.test.js
@@ -0,0 +1,12 @@
+import {Core} from '../../core/core/core.js';
+
+const core = new Core();
+const lineTypeManager = core.ltypeManager;
+
+test('Test LineTypeManager.getStyles', () => {
+  const layers = lineTypeManager.getOptionalStyles();
+  expect(layers).toHaveLength(6);
+
+  expect(layers[0]).toHaveProperty('name', 'CONTINUOUS');
+  expect(layers[1]).toHaveProperty('name', 'CENTER');
+});
