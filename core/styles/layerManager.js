a6cf5105fecbc7aa0e0cef26d28cfbfb28bac72e
Daniel Wood <s.d.wood.82@googlemail.com>
2024-01-16 16:07:47 +0000

    use new colour class for default layers

--- core/styles/layerManager.js
+++ core/styles/layerManager.js
@@ -1,6 +1,8 @@
+import {Colour} from '../lib/colour.js';
 import {DXFFile} from '../lib/dxf/dxfFile.js';
 import {Layer} from './layer.js';
 import {StyleManagerBase} from './styleManagerBase.js';
+
 
 export class LayerManager extends StyleManagerBase {
   constructor() {
@@ -15,7 +17,7 @@ export class LayerManager extends StyleManagerBase {
   }
 
   addStandardStyles() {
-    this.addStyle({'name': '0', 'colour': '#00BFFF'});
+    this.addStyle({'name': '0', 'colour': new Colour(4)});
     this.addStyle({'name': 'DEFPOINTS', 'plotting': false});
     // this.addStyle({'name': 'CENTERLINE', 'colour': '#FFFF00', 'lineType': 'CENTER'});
     // this.addStyle({'name': 'HIDDEN', 'colour': '#D6D6D6', 'lineType': 'HIDDEN'});
