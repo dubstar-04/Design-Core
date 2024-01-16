66727a671ce629ca26c5287a930defb9a6b2e889
Daniel Wood <s.d.wood.82@googlemail.com>
2024-01-16 16:07:53 +0000

    Update colours.test.js

--- test/lib/colours.test.js
+++ test/lib/colours.test.js
@@ -1,29 +1,53 @@
 
 import {Colours} from '../../core/lib/colours.js';
 
-test('Test Colours.getHexColour', () => {
-  expect(Colours.getHexColour(0)).toBe('BYBLOCK');
-  expect(Colours.getHexColour(255)).toBe('#FFFFFF');
+test('Test Colours.isRGB', () => {
+  expect(Colours.isRGB({r: 255, g: 255, b: 255})).toEqual(true);
+  expect(Colours.isRGB({r: 255, b: 255})).toEqual(false);
 });
 
-test('Test Colours.getACADColour', () => {
-  expect(Colours.getACADColour('#FF0000')).toBe('1');
-  expect(Colours.getACADColour('#00FF3F')).toBe('100');
+test('Test Colours.aciToRGB', () => {
+  expect(Colours.aciToRGB(0)).toEqual({r: 0, g: 0, b: 0});
+  expect(Colours.aciToRGB(255)).toEqual({r: 255, g: 255, b: 255});
+
+  // aci 7 should change colour depending on the background colour
+  expect(Colours.aciToRGB(255)).toEqual({r: 255, g: 255, b: 255});
 });
 
-test('Test Colours.rgbToHex', () => {
-  expect(Colours.rgbToHex(1, 1, 1)).toBe('#010101');
-  expect(Colours.rgbToHex(100, 100, 100)).toBe('#646464');
+test('Test Colours.rgbToACI', () => {
+  expect(Colours.rgbToACI({r: 0, g: 0, b: 0})).toEqual(0);
+  expect(Colours.rgbToACI({r: 255, g: 255, b: 255})).toEqual(7);
 });
 
-test('Test Colours.hexToRGB', () => {
-  expect(Colours.hexToRGB('#010101')).toEqual({r: 1, g: 1, b: 1});
-  expect(Colours.hexToRGB('#646464')).toEqual({r: 100, g: 100, b: 100});
-  expect(Colours.hexToRGB('12345')).toBe(null);
+test('Test Colours.rgbToString', () => {
+  expect(Colours.rgbToString({r: 0, g: 0, b: 0})).toEqual('rgb(0, 0, 0)');
+  expect(Colours.rgbToString({r: 255, g: 255, b: 255})).toEqual('rgb(255, 255, 255)');
 });
 
-test('Test Colours.hexToScaledRGB', () => {
-  expect(Colours.hexToScaledRGB('#010101')).toEqual({r: 1/255, g: 1/255, b: 1/255});
-  expect(Colours.hexToScaledRGB('#646464')).toEqual({r: 100/255, g: 100/255, b: 100/255});
-  expect(Colours.hexToScaledRGB('12345')).toBe(null);
+test('Test Colours.rgbToScaledRGB', () => {
+  expect(Colours.rgbToScaledRGB({r: 255, g: 255, b: 255})).toEqual({r: 1, g: 1, b: 1});
+  expect(Colours.rgbToScaledRGB({r: 100, g: 100, b: 100})).toEqual({r: 100/255, g: 100/255, b: 100/255});
+  expect(Colours.rgbToScaledRGB({r: 1, g: 1})).toBeUndefined();
+  expect(Colours.rgbToScaledRGB({})).toBeUndefined();
+});
+
+test('Test Colours.trueColourToRGB', () => {
+  // rgb(200, 100,50)
+  // hex 0x00C86432
+  // int 13132850
+  const rgb = Colours.trueColourToRGB(13132850);
+  expect(rgb.r).toBe(200);
+  expect(rgb.g).toBe(100);
+  expect(rgb.b).toBe(50);
+
+  expect(Colours.trueColourToRGB()).toBeUndefined();
+});
+
+
+test('Test Colours.rgbToTrueColour', () => {
+  // rgb(200, 100,50)
+  // hex 0x00C86432
+  // int 13132850
+  expect(Colours.rgbToTrueColour({r: 200, g: 100, b: 50})).toBe(13132850);
+  expect(Colours.rgbToTrueColour()).toBeUndefined();
 });
