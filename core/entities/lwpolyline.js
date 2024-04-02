161fce9c63bf9349bbd7d208313816bfc9d167dd
Daniel Wood <s.d.wood.82@googlemail.com>
2024-04-02 17:23:34 +0100

    Clean up

--- core/entities/lwpolyline.js
+++ core/entities/lwpolyline.js
@@ -19,44 +19,4 @@ export class Lwpolyline extends BasePolyline {
     const command = {command: 'Lwpolyline'};
     return command;
   }
-
-  // dxf
-  // R12 doesn't support Lwpolyline
-  // output as polyline definition by default from super class
-  /*
-  dxf() {
-    let pointData = '';
-    this.points.forEach((point) => {
-      pointData = pointData.concat(
-          '\n', '10', // X
-          '\n', point.x,
-          '\n', '20', // Y
-          '\n', point.y,
-          '\n', '30', // Z
-          '\n', '0',
-          '\n', '42', // bulge
-          '\n', point.bulge,
-      );
-    });
-
-    const dxfitem = '';
-    const data = dxfitem.concat(
-        '0',
-        '\n', 'LWPOLYLINE',
-        // "\n", "5", //HANDLE
-        // "\n", "DA",
-        '\n', '8', // LAYERNAME
-        '\n', this.layer,
-        '\n', '39', // Line Width
-        '\n', this.lineWidth,
-        '\n', '70', // Flags
-        '\n', this.flags,
-        '\n', '90', // Vertex count
-        '\n', this.points.length,
-        pointData,
-
-    );
-    return data;
-  }
-  */
 }
