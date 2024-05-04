7808c19a730bae8538c67bc265781aad1fb086bf
Daniel Wood <s.d.wood.82@googlemail.com>
2024-05-04 11:05:07 +0100

    Update style.test.js

--- test/styles/style.test.js
+++ test/styles/style.test.js
@@ -3,31 +3,42 @@ import {Style} from '../../core/styles/style';
 test('Test Style.vertical', () => {
   const style = new Style();
   expect(style.vertical).toBe(false);
-  expect(style.standardFlags).toBe(0);
+  expect(style.standardFlags.getFlagValue()).toBe(0);
 
   style.vertical = true;
   expect(style.vertical).toBe(true);
-  expect(style.standardFlags).toBe(4);
+  expect(style.standardFlags.hasFlag(4)).toBe(true);
+  expect(style.standardFlags.getFlagValue()).toBe(4);
 });
 
 
 test('Test Style.backwards', () => {
   const style = new Style();
   expect(style.backwards).toBe(false);
-  expect(style.flags).toBe(0);
+  expect(style.flags.getFlagValue()).toBe(0);
 
   style.backwards = true;
   expect(style.backwards).toBe(true);
-  expect(style.flags).toBe(2);
+  expect(style.flags.hasFlag(2)).toBe(true);
+  expect(style.flags.getFlagValue()).toBe(2);
+
+  style.backwards = false;
+  expect(style.backwards).toBe(false);
+  expect(style.flags.hasFlag(2)).toBe(false);
 });
 
 test('Test Style.upsideDown', () => {
   const style = new Style();
   expect(style.upsideDown).toBe(false);
-  expect(style.flags).toBe(0);
+  expect(style.flags.getFlagValue()).toBe(0);
 
   style.upsideDown = true;
   expect(style.upsideDown).toBe(true);
-  expect(style.flags).toBe(4);
+  expect(style.flags.hasFlag(4)).toBe(true);
+  expect(style.flags.getFlagValue()).toBe(4);
+
+  style.upsideDown = false;
+  expect(style.upsideDown).toBe(false);
+  expect(style.flags.hasFlag(4)).toBe(false);
 });
 
