cc53d7a68a1cd4692053dbc106abb12ca22b32a3
Daniel Wood <s.d.wood.82@googlemail.com>
2024-05-27 16:15:39 +0100

    output purged item count

--- core/tools/purge.js
+++ core/tools/purge.js
@@ -33,18 +33,29 @@ export class Purge extends Tool {
   }
 
   action() {
+    let purgedCount = 0;
+
     if (this.option === 'Blocks' || this.option === 'All') {
+      const intialBlockCount = DesignCore.Scene.blockManager.blockCount();
       DesignCore.Scene.blockManager.purge();
+      const finalBlockCount = DesignCore.Scene.blockManager.blockCount();
+      purgedCount += (intialBlockCount - finalBlockCount);
     }
 
     if (this.option === 'Layers'|| this.option === 'All') {
+      const intialLayerCount = DesignCore.LayerManager.styleCount();
       DesignCore.LayerManager.purge();
+      const finalLayerCount = DesignCore.LayerManager.styleCount();
+      purgedCount += (intialLayerCount - finalLayerCount);
     }
 
     if (this.option === 'LTypes'|| this.option === 'All') {
+      const intialLTypeCount = DesignCore.LTypeManager.styleCount();
       DesignCore.LTypeManager.purge();
+      const finalLTypeCount = DesignCore.LTypeManager.styleCount();
+      purgedCount += (intialLTypeCount - finalLTypeCount);
     }
 
-    DesignCore.Core.notify(`${this.type} ${this.option}: ${Strings.Message.COMPLETED}`);
+    DesignCore.Core.notify(`${this.type} ${this.option}: ${purgedCount} ${Strings.Strings.ITEMS} ${Strings.Strings.REMOVED}`);
   }
 }
