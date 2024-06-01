23b42b21e9109758399959efcd09fdc1e01f483d
Daniel Wood <s.d.wood.82@googlemail.com>
2024-06-01 16:34:56 +0100

    Add missing jsdocs to fileIO

--- core/lib/fileio.js
+++ core/lib/fileio.js
@@ -6,12 +6,21 @@ import {DesignCore} from '../designCore.js';
 
 /** FileIO Class */
 export class FileIO {
+  /**
+   * Save to DXF
+   * @param {String} version
+   * @returns {String} dxf file as a string
+   */
   static saveDxf(version) {
     const dxfWriter = new DXF();
     const data = dxfWriter.write(version);
     return data;
   }
 
+  /**
+   * Open data as a dxf file
+   * @param {String} data
+   */
   static openFile(data) {
     const dxf = new DXF();
     try {
