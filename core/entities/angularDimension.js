bc2fcb68019949fc60a33069abde208bfad6d5a7
Daniel Wood <s.d.wood.82@googlemail.com>
2025-07-29 16:21:43 +0100

    match point number to sequence number

--- core/entities/angularDimension.js
+++ core/entities/angularDimension.js
@@ -44,18 +44,20 @@ export class AngularDimension extends BaseDimension {
     try {
       const op = new PromptOptions(Strings.Input.SELECT, [Input.Type.SINGLESELECTION]);
 
+      this.dimensionStyle = DesignCore.DimStyleManager.getCstyle()
+
       if (!DesignCore.Scene.selectionManager.selectionSet.selectionSet.length) {
         const selection = await DesignCore.Scene.inputManager.requestInput(op);
 
         const line1 = DesignCore.Scene.getItem(selection.selectedItemIndex);
 
-        const pt15 = line1.points[0];
-        pt15.sequence = 15;
-        this.points.push(pt15);
+        const Pt155 = line1.points[0];
+        Pt155.sequence = 15;
+        this.points.push(Pt155);
 
-        const pt10 = line1.points[1];
-        pt10.sequence = 10;
-        this.points.push(pt10);
+        const Pt150 = line1.points[1];
+        Pt150.sequence = 10;
+        this.points.push(Pt150);
 
         DesignCore.Scene.selectionManager.reset();
       }
@@ -68,28 +70,28 @@ export class AngularDimension extends BaseDimension {
 
         const line2 = DesignCore.Scene.getItem(selection2.selectedItemIndex);
 
-        const pt13 = line2.points[0];
-        pt13.sequence = 13;
-        this.points.push(pt13);
+        const Pt153 = line2.points[0];
+        Pt153.sequence = 13;
+        this.points.push(Pt153);
 
-        const pt14 = line2.points[1];
-        pt14.sequence = 14;
-        this.points.push(pt14);
+        const Pt154 = line2.points[1];
+        Pt154.sequence = 14;
+        this.points.push(Pt154);
 
         DesignCore.Scene.selectionManager.reset();
       }
 
       const op2 = new PromptOptions(Strings.Input.START, [Input.Type.POINT]);
-      const pt11 = await DesignCore.Scene.inputManager.requestInput(op2);
-      pt11.sequence = 11;
-      this.points.push(pt11);
+      const Pt151 = await DesignCore.Scene.inputManager.requestInput(op2);
+      Pt151.sequence = 11;
+      this.points.push(Pt151);
 
-      const pt15 = this.points.find((point) => point === 15);
-      const pt10 = this.getPointBySequence(10);
+      const Pt155 = this.points.find((point) => point === 15);
+      const Pt150 = this.getPointBySequence(10);
 
-      const pt16 = pt11.perpendicular(pt15, pt10);
-      pt16.sequence = 16;
-      this.points.push(pt16);
+      const Pt156 = Pt151.perpendicular(Pt155, Pt150);
+      Pt156.sequence = 16;
+      this.points.push(Pt156);
 
       DesignCore.Scene.inputManager.executeCommand(this);
     } catch (err) {
@@ -105,13 +107,13 @@ export class AngularDimension extends BaseDimension {
       const mousePoint = DesignCore.Mouse.pointOnScene();
       mousePoint.sequence = 11;
 
-      const pt15 = this.getPointBySequence(15);
-      const pt10 = this.getPointBySequence(10);
+      const Pt155 = this.getPointBySequence(15);
+      const Pt150 = this.getPointBySequence(10);
 
-      const pt16 = mousePoint.perpendicular(pt15, pt10);
-      pt16.sequence = 16;
+      const Pt156 = mousePoint.perpendicular(Pt155, Pt150);
+      Pt156.sequence = 16;
 
-      const points = [...this.points, mousePoint, pt16];
+      const points = [...this.points, mousePoint, Pt156];
       DesignCore.Scene.createTempItem(this.type, { points: points });
     }
   }
@@ -127,21 +129,21 @@ export class AngularDimension extends BaseDimension {
     const item1 = items[0];
     const item2 = items[1];
 
-    const pt15 = item1.points[0];
-    pt15.sequence = 15;
-    points.push(pt15);
+    const Pt155 = item1.points[0];
+    Pt155.sequence = 15;
+    points.push(Pt155);
 
-    const pt10 = item1.points[1];
-    pt10.sequence = 10;
-    points.push(pt10);
+    const Pt150 = item1.points[1];
+    Pt150.sequence = 10;
+    points.push(Pt150);
 
-    const pt13 = item2.points[0];
-    pt13.sequence = 13;
-    points.push(pt13);
+    const Pt153 = item2.points[0];
+    Pt153.sequence = 13;
+    points.push(Pt153);
 
-    const pt14 = item2.points[1];
-    pt14.sequence = 14;
-    points.push(pt14);
+    const Pt154 = item2.points[1];
+    Pt154.sequence = 14;
+    points.push(Pt154);
 
     return points;
   }
@@ -156,31 +158,35 @@ export class AngularDimension extends BaseDimension {
     let dimension = 0;
     const entities = [];
 
-    const Pt1 = this.getPointBySequence(15);
-    const Pt2 = this.getPointBySequence(10);
-    const Pt3 = this.getPointBySequence(13);
-    const Pt4 = this.getPointBySequence(14);
-    const Pt5 = this.getPointBySequence(11); // 16); // should be point 16 changed to debug
-    const Pt6 = this.getPointBySequence(11);
+    const Pt15 = this.getPointBySequence(15);
+    const Pt10 = this.getPointBySequence(10);
+    const Pt13 = this.getPointBySequence(13);
+    const Pt14 = this.getPointBySequence(14);
+    const Pt16 = this.getPointBySequence(16);
+    const Pt11 = this.getPointBySequence(11);
 
-    const line1 = new Line({ points: [Pt1, Pt2] });
-    const line2 = new Line({ points: [Pt3, Pt4] });
-    const arrow1pos = Pt5.perpendicular(Pt1, Pt2);
-    const arrow2pos = Pt5.perpendicular(Pt3, Pt4);
-    const arrowHead1 = this.getArrowHead(arrow1pos, Pt1.angle(Pt2) + 1.5708, this.text.height / 2);
-    const arrowHead2 = this.getArrowHead(arrow2pos, Pt3.angle(Pt4) + 1.5708, this.text.height / 2);
+    const line1 = new Line({ points: [Pt15, Pt10] });
+    const line2 = new Line({ points: [Pt13, Pt14] });
 
-    const intersect = Intersection.intersectLineLine({ start: Pt1, end: Pt2 }, { start: Pt3, end: Pt4 }, true);
+    const arrow1pos = Pt16.perpendicular(Pt15, Pt10);
+    const arrow2pos = Pt16.perpendicular(Pt13, Pt14);
+
+    const arrowHead1 = this.getArrowHead(arrow1pos, Pt15.angle(Pt10) + 1.5708, this.text.height / 2);
+    const arrowHead2 = this.getArrowHead(arrow2pos, Pt13.angle(Pt14) + 1.5708, this.text.height / 2);
+
+    const intersect = Intersection.intersectLineLine({ start: Pt15, end: Pt10 }, { start: Pt13, end: Pt14 }, true);
     const arc = new Arc({ points: [intersect.points[0], arrow1pos, arrow2pos] });
 
     entities.push(line1, line2, arrowHead1, arrowHead2, arc);
 
-    this.text.points = [Pt6];
-    // this.text.setRotation(Pt1.angle(Pt2));
-    const angle = Utils.radians2degrees(Pt1.angle(Pt2));
+    this.text.points = [Pt11];
+    // this.text.setRotation(Pt15.angle(Pt10));
+    const angle = Utils.radians2degrees(Pt15.angle(Pt10));
     this.text.rotation = angle;
-    const line1Angle = Pt1.angle(Pt2);
-    const line2Angle = Pt3.angle(Pt4);
+
+    const line1Angle = Pt15.angle(Pt10);
+    const line2Angle = Pt13.angle(Pt14);
+
     dimension = Utils.radians2degrees(line1Angle - line2Angle);
     const precision = style.getValue('DIMADEC') || 2; // Default precision
     this.text.string = `${Math.abs(dimension.toFixed(precision)).toString()}${Strings.Symbol.DEGREE}`;
