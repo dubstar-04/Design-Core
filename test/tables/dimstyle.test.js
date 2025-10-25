e51249578e9d8ac152776de66e254ac2a5d15fa4
Daniel Wood <s.d.wood.82@googlemail.com>
2025-10-25 10:05:08 +0100

    Fix dimstyle tests

--- test/tables/dimstyle.test.js
+++ test/tables/dimstyle.test.js
@@ -1,20 +1,32 @@
 import { DimStyle } from '../../core/tables/dimStyle';
 
-test('Test DimStyle.getValue', () => {
-  const style = new DimStyle();
-  expect(style.getValue('DIMSE1')).toBe(false);
-  expect(style.getValue('DIMASZ')).toBe(2.5);
-});
+describe('DimStyle', () => {
+  let style;
 
-test('Test DimStyle.vertical', () => {
-  const style = new DimStyle();
-  expect(style.vertical).toBe(false);
-  expect(style.standardFlags.getFlagValue()).toBe(0);
+  beforeEach(() => {
+    const data = {};
+    style = new DimStyle(data);
+  });
 
-  style.vertical = true;
-  expect(style.vertical).toBe(true);
-  expect(style.standardFlags.hasFlag(4)).toBe(true);
-  expect(style.standardFlags.getFlagValue()).toBe(4);
+  test('getValue returns correct default values', () => {
+    expect(style.getValue('DIMSE1')).toBe(0); // Suppress first extension line
+    expect(style.getValue('DIMASZ')).toBe(0.18); // Arrow size
+  });
+
+  test('vertical flag getter and setter', () => {
+    expect(style.vertical).toBe(false);
+    expect(style.standardFlags.getFlagValue()).toBe(0);
+
+    style.vertical = true;
+    expect(style.vertical).toBe(true);
+    expect(style.standardFlags.hasFlag(4)).toBe(true);
+    expect(style.standardFlags.getFlagValue()).toBe(4);
+
+    style.vertical = false;
+    expect(style.vertical).toBe(false);
+    expect(style.standardFlags.hasFlag(4)).toBe(false);
+    expect(style.standardFlags.getFlagValue()).toBe(0);
+  });
 });
 
 
