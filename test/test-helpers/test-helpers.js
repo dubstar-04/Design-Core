d9bb52966e915fc10ecc7dc1b5b50efdfe8ce8fa
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-08 13:52:09 +0000

    Update test-helpers.js

--- test/test-helpers/test-helpers.js
+++ test/test-helpers/test-helpers.js
@@ -1,14 +1,28 @@
 
-// mock file for dxf test
+/**
+ *
+ */
 export class File {
+  /**
+   * Mock File class constructor
+   */
   constructor() {
     this.contents = '';
   }
 
+  /**
+   * Mock function to return next handle
+   * @returns number
+   */
   nextHandle() {
     return 1;
   }
 
+  /**
+   * Mock function to write group code and value
+   * @param {number} groupCode
+   * @param {any} groupValue
+   */
   writeGroupCode(groupCode, groupValue) {
     this.contents = this.contents.concat(`${groupCode}\n${groupValue}\n`);
   }
