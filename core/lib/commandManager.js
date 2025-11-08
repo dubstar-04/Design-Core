9b14be883de928d5ecc673ab8c48ba1c7de76f7d
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-08 12:57:14 +0000

    Disable Dimension Commands

--- core/lib/commandManager.js
+++ core/lib/commandManager.js
@@ -1,16 +1,16 @@
-import { Line } from '../entities/line.js';
-import { Polyline } from '../entities/polyline.js';
-import { Lwpolyline } from '../entities/lwpolyline.js';
-import { Circle } from '../entities/circle.js';
-import { Arc } from '../entities/arc.js';
-import { Rectangle } from '../entities/rectangle.js';
+import {Line} from '../entities/line.js';
+import {Polyline} from '../entities/polyline.js';
+import {Lwpolyline} from '../entities/lwpolyline.js';
+import {Circle} from '../entities/circle.js';
+import {Arc} from '../entities/arc.js';
+import {Rectangle} from '../entities/rectangle.js';
 // import {Ellipse} from '../entities/ellipse.js';
 // import {Spline} from '../entities/spline.js';
-import { Text } from '../entities/text.js';
-import { Block } from '../tables/block.js';
-import { Insert } from '../entities/insert.js';
-import { Hatch } from '../entities/hatch.js';
-
+import {Text} from '../entities/text.js';
+import {Block} from '../tables/block.js';
+import {Insert} from '../entities/insert.js';
+import {Hatch} from '../entities/hatch.js';
+/*
 import { Dimension } from '../dimensions/dimension.js';
 import { AlignedDimension } from '../dimensions/alignedDimension.js';
 import { AngularDimension } from '../dimensions/angularDimension.js';
@@ -18,25 +18,26 @@ import { LinearDimension } from '../dimensions/linearDimension.js';
 import { DiametricDimension } from '../dimensions/diametricDimension.js';
 import { RadialDimension } from '../dimensions/radialDimension.js';
 import { RotatedDimension } from '../dimensions/rotatedDimension.js';
+*/
 
 // import { Point } from "../entities/point.js"
 
-import { Move } from '../tools/move.js';
-import { Copy } from '../tools/copy.js';
-import { Rotate } from '../tools/rotate.js';
-import { Erase } from '../tools/erase.js';
-import { Explode } from '../tools/explode.js';
-import { Distance } from '../tools/distance.js';
-import { Identify } from '../tools/identify.js';
-import { Extend } from '../tools/extend.js';
-import { Trim } from '../tools/trim.js';
-import { Purge } from '../tools/purge.js';
+import {Move} from '../tools/move.js';
+import {Copy} from '../tools/copy.js';
+import {Rotate} from '../tools/rotate.js';
+import {Erase} from '../tools/erase.js';
+import {Explode} from '../tools/explode.js';
+import {Distance} from '../tools/distance.js';
+import {Identify} from '../tools/identify.js';
+import {Extend} from '../tools/extend.js';
+import {Trim} from '../tools/trim.js';
+import {Purge} from '../tools/purge.js';
 
-import { Utils } from './utils.js';
-import { Strings } from './strings.js';
-import { Logging } from './logging.js';
+import {Utils} from './utils.js';
+import {Strings} from './strings.js';
+import {Logging} from './logging.js';
 
-import { DesignCore } from '../designCore.js';
+import {DesignCore} from '../designCore.js';
 
 
 const classes = {
@@ -49,6 +50,7 @@ const classes = {
   // Ellipse,
   // Spline,
   Text,
+  /*
   Dimension,
   AngularDimension,
   AlignedDimension,
@@ -56,6 +58,7 @@ const classes = {
   LinearDimension,
   RadialDimension,
   RotatedDimension,
+  */
   Block,
   Insert,
   Hatch,
