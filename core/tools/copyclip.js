4f1c9323bf2e990a5f08f10ef9679085ed813694
Daniel Wood <s.d.wood.82@googlemail.com>
2025-11-22 15:27:48 +0000

    Create copyclip.js

---
+++ core/tools/copyclip.js
@@ -0,0 +1,61 @@
+import { Strings } from '../lib/strings.js';
+import { Tool } from './tool.js';
+import { Input, PromptOptions } from '../lib/inputManager.js';
+import { Logging } from '../lib/logging.js';
+
+import { DesignCore } from '../designCore.js';
+
+/**
+ * Copyclip Command Class
+ * @extends Tool
+ */
+export class Copyclip extends Tool {
+  /** Create a Copyclip command */
+  constructor() {
+    super();
+  }
+
+  /**
+   * Register the command
+   * @return {Object}
+   * command = name of the command
+   * shortcut = shortcut for the command
+   * type = type to group command in toolbars (omitted if not shown)
+   */
+  static register() {
+    const command = { command: 'Copyclip', shortcut: 'COPYCLIP' };
+    return command;
+  }
+
+  /**
+   * Execute method
+   * executes the workflow, requesting input required to perform the command
+   */
+  async execute() {
+    try {
+      const op = new PromptOptions(Strings.Input.SELECTIONSET, [Input.Type.SELECTIONSET]);
+
+      if (!DesignCore.Scene.selectionManager.selectionSet.selectionSet.length) {
+        await DesignCore.Scene.inputManager.requestInput(op);
+      }
+
+      DesignCore.Scene.inputManager.executeCommand();
+    } catch (err) {
+      Logging.instance.error(`${this.type} - ${err}`);
+    }
+  }
+
+
+  /**
+   * Perform the command
+   */
+  action() {
+    console.log('Copyclip action performed');
+
+    // generate clipboard data from selected items
+    const selectedItems = DesignCore.Scene.selectionManager.selectedItems;
+
+    // create clipboard data
+    DesignCore.Clipboard.Entities = selectedItems;
+  };
+}
