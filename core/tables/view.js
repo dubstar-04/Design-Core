ac02a485ec2be4973272a65168b6e83d51a2a18c
Daniel Wood <s.d.wood.82@googlemail.com>
2026-03-20 15:56:03 +0000

    Create view.js

---
+++ core/tables/view.js
@@ -0,0 +1,30 @@
+import { DXFFile } from '../lib/dxf/dxfFile.js';
+
+/** View Class */
+export class View {
+  /**
+   * Create a View
+   * @param {Object} data
+   */
+  constructor(data) {
+    this.name = '';
+
+    if (data) {
+      if (data.hasOwnProperty('name') || data.hasOwnProperty('2')) {
+        this.name = data.name || data[2];
+      }
+    }
+  }
+
+  /**
+   * Write the item to file in the dxf format
+   * @param {DXFFile} file
+   */
+  dxf(file) {
+    file.writeGroupCode('0', 'VIEW', DXFFile.Version.R2000);
+    file.writeGroupCode('5', file.nextHandle(), DXFFile.Version.R2000);
+    file.writeGroupCode('100', 'AcDbSymbolTableRecord', DXFFile.Version.R2000);
+    file.writeGroupCode('100', 'AcDbViewTableRecord', DXFFile.Version.R2000);
+    file.writeGroupCode('2', this.name, DXFFile.Version.R2000);
+  }
+}
