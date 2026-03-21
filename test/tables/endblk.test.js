e235685d03f7650c61f778aea5b394e559e6b82c
Daniel Wood <s.d.wood.82@googlemail.com>
2026-03-21 07:35:01 +0000

    Create endblk.test.js

---
+++ test/tables/endblk.test.js
@@ -0,0 +1,30 @@
+import { EndBlock } from '../../core/tables/endblk.js';
+import { File } from '../test-helpers/test-helpers.js';
+
+test('Test EndBlock constructor defaults', () => {
+  const endblk = new EndBlock();
+  expect(endblk.layer).toBe('0');
+});
+
+test('Test EndBlock constructor with layer', () => {
+  const endblk = new EndBlock({ 8: 'TestLayer' });
+  expect(endblk.layer).toBe('TestLayer');
+});
+
+test('Test EndBlock.dxf', () => {
+  const endblk = new EndBlock();
+  const file = new File();
+  endblk.dxf(file);
+
+  const dxfString = `0
+ENDBLK
+5
+1
+100
+AcDbEntity
+100
+AcDbBlockEnd
+`;
+
+  expect(file.contents).toEqual(dxfString);
+});
