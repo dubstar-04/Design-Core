29b02aa7851018bc03ade87dbacc6ff6c7e8fd1c
Daniel Wood <s.d.wood.82@googlemail.com>
2026-03-22 14:14:40 +0000

    Parse DXF Objects

---
+++ core/lib/dxf/sections/objects.js
@@ -0,0 +1,36 @@
+import {Section} from './section.js';
+
+/**
+ * Objects Class
+ * @extends Section
+ */
+export class Objects extends Section {
+  /** Create Objects */
+  constructor() {
+    super();
+
+    this.objects = [];
+  }
+
+  /**
+   * Read
+   * @param {Object} iterator
+   * @return {Array}
+   */
+  read(iterator) {
+    while (iterator.nextPair().value !== 'ENDSEC') {
+      const currentPair = iterator.currentPair();
+      switch (true) {
+        case (currentPair.code === '0'):
+          const object = this.parseChild(iterator);
+          this.objects.push(object);
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    return this.objects;
+  }
+}
