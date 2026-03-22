459683c04d25a023410f6ca4bac95d65e5ebef40
Daniel Wood <s.d.wood.82@googlemail.com>
2026-03-22 12:34:07 +0000

    Load view data

--- core/tables/viewManager.js
+++ core/tables/viewManager.js
@@ -33,6 +33,12 @@ export class ViewManager extends TableManagerBase {
     file.writeGroupCode('2', 'VIEW', DXFFile.Version.R2000);
     file.writeGroupCode('5', this.handle, DXFFile.Version.R2000);
     file.writeGroupCode('100', 'AcDbSymbolTable', DXFFile.Version.R2000);
+    file.writeGroupCode('70', this.itemCount(), DXFFile.Version.R2000);
+
+    for (let i = 0; i < this.itemCount(); i++) {
+      this.getItemByIndex(i).dxf(file);
+    }
+
     file.writeGroupCode('0', 'ENDTAB', DXFFile.Version.R2000);
   }
 }
