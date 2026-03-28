6bfdfd1d3fd002caf7d500e80105348ec118771e
Daniel Wood <s.d.wood.82@googlemail.com>
2026-03-28 20:08:42 +0000

    use point methods

--- core/tools/distance.js
+++ core/tools/distance.js
@@ -1,4 +1,5 @@
 import { Strings } from '../lib/strings.js';
+import { Utils } from '../lib/utils.js';
 import { Tool } from './tool.js';
 import { Input, PromptOptions } from '../lib/inputManager.js';
 import { Logging } from '../lib/logging.js';
@@ -63,7 +64,7 @@ export class Distance extends Tool {
     const dx = this.points[1].x - this.points[0].x;
     const dy = this.points[1].y - this.points[0].y;
     const length = this.points[0].distance(this.points[1]).toFixed(1);
-    const angle = (Math.atan2(dy, dx) * 180 / Math.PI).toFixed(1);
+    const angle = Utils.radians2degrees(this.points[0].angle(this.points[1])).toFixed(1);
     const x = dx.toFixed(1);
     const y = dy.toFixed(1);
     const di = (`${Strings.Strings.LENGTH}: ${length} Angle: ${angle}${Strings.Symbol.DEGREE} ${Strings.Symbol.DELTA}X: ${x} ${Strings.Symbol.DELTA}Y: ${y}`);
