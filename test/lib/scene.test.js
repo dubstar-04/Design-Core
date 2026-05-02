ba9e5ab28db8039da91b1044bea4d438eee9ff5d
Daniel Wood <s.d.wood.82@googlemail.com>
2026-05-02 17:40:16 +0100

    Update scene.test.js

--- test/lib/scene.test.js
+++ test/lib/scene.test.js
@@ -11,16 +11,16 @@ test('Test Scene.boundingBox', () => {
   expect(scene.boundingBox()).toBeUndefined();
 
   const line1 = new Line({ points: [new Point(-102, -102), new Point(201, 202)] });
-  // add to scene (bypass scene.addItem())
-  scene.addItem('Line', line1);
+  // add to scene (bypass scene.addEntity())
+  scene.addEntity('Line', line1);
   expect(scene.boundingBox().xMin).toBeCloseTo(-102);
   expect(scene.boundingBox().xMax).toBeCloseTo(201);
   expect(scene.boundingBox().yMin).toBeCloseTo(-102);
   expect(scene.boundingBox().yMax).toBeCloseTo(202);
 
   const line2 = new Line({ points: [new Point(1001, -2002), new Point(-2001, 2002)] });
-  // add to scene (bypass scene.addItem())
-  scene.addItem('Line', line2);
+  // add to scene (bypass scene.addEntity())
+  scene.addEntity('Line', line2);
   expect(scene.boundingBox().xMin).toBeCloseTo(-2001);
   expect(scene.boundingBox().xMax).toBeCloseTo(1001);
   expect(scene.boundingBox().yMin).toBeCloseTo(-2002);
@@ -28,26 +28,26 @@ test('Test Scene.boundingBox', () => {
 });
 
 
-test('Test Scene.addItem', () => {
+test('Test Scene.addEntity', () => {
   // Throw error on missing data
   expect(() => {
-    scene.addItem('Line');
+    scene.addEntity('Line');
   }).toThrow();
 
   // Add a invalid item type
   const itemCnt = scene.entities.count();
-  const returnIndex = scene.addItem('FakeItem', {});
+  const returnIndex = scene.addEntity('FakeItem', {});
   expect(scene.entities.count()).toBe(itemCnt);
   expect(returnIndex).toBeUndefined();
 
   // Add a valid item with data
   const itemCount = scene.entities.count();
-  const index = scene.addItem('Line', { points: [new Point(), new Point(10, 10)] });
+  const index = scene.addEntity('Line', { points: [new Point(), new Point(10, 10)] });
   expect(scene.entities.count()).toBe(itemCount + 1);
   expect(index).toBe(itemCount);
 
   // Add a valid item with data at index - item count should not increase
-  scene.addItem('Line', { points: [new Point(123, 123), new Point(321, 321)] }, index);
+  scene.addEntity('Line', { points: [new Point(123, 123), new Point(321, 321)] }, index);
   expect(scene.entities.count()).toBe(itemCount + 1);
   // check the items points to make sure index was replaced
   expect(scene.entities.get(index).points[0].x).toBe(123);
