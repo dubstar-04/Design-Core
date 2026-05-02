2cc7a727079b62f480aac7326b57a21564c105b9
Daniel Wood <s.d.wood.82@googlemail.com>
2026-05-02 17:41:27 +0100

    Update rotate.test.js

--- test/tools/rotate.test.js
+++ test/tools/rotate.test.js
@@ -49,7 +49,7 @@ test.each(inputScenarios)('Rotate.execute handles $desc', async (scenario) => {
   // create line
   const pointOne = new Point(10, 0);
   const pointTwo = new Point(20, 0);
-  core.scene.addItem('Line', { points: [pointOne, pointTwo] });
+  core.scene.addEntity('Line', { points: [pointOne, pointTwo] });
 
   // select line
   core.scene.selectionManager.addToSelectionSet(0);
@@ -78,12 +78,12 @@ test.each(inputScenarios)('Rotate.execute handles $desc', async (scenario) => {
 
 test('Test Rotate.action', () => {
   // Add items to scene
-  core.scene.addItem('Line', { points: [new Point(10, 0), new Point(20, 0)] });
-  core.scene.addItem('Circle', { points: [new Point(10, 0), new Point(20, 0)] });
-  core.scene.addItem('Polyline', { points: [new Point(10, 0), new Point(20, 0)] });
-  core.scene.addItem('Arc', { points: [new Point(10, 0), new Point(10, 10), new Point(0, 10)] });
-  core.scene.addItem('Rectangle', { points: [new Point(10, 0), new Point(0, 10)] });
-  core.scene.addItem('Text', { points: [new Point(10, 0), new Point(0, 10)], height: 10, rotation: 0, string: 'text test' });
+  core.scene.addEntity('Line', { points: [new Point(10, 0), new Point(20, 0)] });
+  core.scene.addEntity('Circle', { points: [new Point(10, 0), new Point(20, 0)] });
+  core.scene.addEntity('Polyline', { points: [new Point(10, 0), new Point(20, 0)] });
+  core.scene.addEntity('Arc', { points: [new Point(10, 0), new Point(10, 10), new Point(0, 10)] });
+  core.scene.addEntity('Rectangle', { points: [new Point(10, 0), new Point(0, 10)] });
+  core.scene.addEntity('Text', { points: [new Point(10, 0), new Point(0, 10)], height: 10, rotation: 0, string: 'text test' });
 
   // Add items to selection set
   for (let i = 0; i < core.scene.entities.count(); i++) {
@@ -149,7 +149,7 @@ test('Rotate.execute with reference - resets inputPoint to rotation centre', asy
   core.scene.inputManager = mockInputManager;
 
   core.scene.clear();
-  core.scene.addItem('Line', { points: [new Point(10, 0), new Point(20, 0)] });
+  core.scene.addEntity('Line', { points: [new Point(10, 0), new Point(20, 0)] });
   core.scene.selectionManager.addToSelectionSet(0);
 
   const rotate = new Rotate();
