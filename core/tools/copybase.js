4f9dbc9ed87c32ad30c23b7cfbb0634ddf6abca5
Daniel Wood <s.d.wood.82@googlemail.com>
2026-05-02 17:38:36 +0100

    Consistently use Entity not Item

--- core/tools/copybase.js
+++ core/tools/copybase.js
@@ -57,7 +57,7 @@ export class Copybase extends Tool {
    */
   action() {
     // generate clipboard data from selected items
-    const selectedItems = DesignCore.Scene.selectionManager.selectedItems;
+    const selectedItems = DesignCore.Scene.selectionManager.selectedEntities;
 
     // create clipboard data
     DesignCore.Clipboard.Entities = selectedItems;
